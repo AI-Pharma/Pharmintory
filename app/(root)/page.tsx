f27e13d9a0b99d4aa0506cea473ac52605587b33
Prince <iamprinceasante@gmail.com>
2025-11-22 02:53:16 +0000

    feature section modified and responsive

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -6,11 +6,11 @@ import Image from "next/image";
 
 const Landing = () => {
   return (
-    <main className="flex flex-col items-center inter-font py-[30px] py:[48px] lg:py-[60px]">
+    <main className="flex flex-col items-center inter-font gap-y-8 py-[30px] md:py-[48px] lg:py-[60px]">
       {/* Hero Section */}
-      <section className="flex items-center justify-center flex-col gap-y-[1rem] md:gap-y-[2rem] px-4 md:px-[30px]">
+      <section className="flex items-center justify-center flex-col gap-y-[1rem] md:gap-y-[2rem] px-4 md:px-[30px] w-full">
         <div className="flex flex-col md:w-lg lg:w-[39.6875rem] items-center gap-y-4 lg:gap-[1.4375rem]">
-          <h1 className="text-center text-xl font-semibold tracking-[-0.0271rem] text-cyanText urbanist-font capitalize">
+          <h1 className="text-center text-2xl font-semibold tracking-[-0.0271rem] text-cyanText urbanist-font capitalize">
             AI PHARMA
           </h1>
           <h2
@@ -53,25 +53,32 @@ const Landing = () => {
         </div>
       </section>
 
-      {/* <div className="flex flex-col w-[48.875rem] items-center gap-[1.4375rem] mb-[4.25rem]">
-        <h4 className="text-center text-2xl font-semibold leading-[2.125rem] tracking-[-0.0271rem] text-cyanText urbanist-font capitalize">
-          AI PHARMA
-        </h4>
-        <p className="text-someBlackColor text-center urbanist-font text-[3.25rem] font-medium leading-[4rem] tracking-[-0.0587rem]">
-          Smart tools for smarter data management
-        </p>
-      </div> */}
+      <section className="flex flex-col pt-[30px] md:pt-[48px] lg:pt-[60px] px-4 md:px-[30px]">
+        <div className="flex flex-col items-center gap-[1.4375rem] mb-[4.25rem]">
+          <h3 className="text-center text-2xl font-semibold leading-[2.125rem] tracking-[-0.0271rem] text-cyanText urbanist-font capitalize">
+            Long Headline Sample
+          </h3>
+          <h4
+            className={cn(
+              "text-someBlackColor text-center urbanist-font font-semibold tracking-[-0.0722rem] md:w-md lg:w-[48.875rem]",
+              "text-3xl md:text-4xl lg:text-[3.2rem] leading-10 md:leading-14 lg:leading-[62px]"
+            )}
+          >
+            Smart tools for smarter data management
+          </h4>
+        </div>
 
-      {/* <div className="flex gap-[1.875rem] mb-[23.1875rem]">
-        {featureTileInfo.map((info, index) => (
-          <FeatureTile
-            key={index}
-            icon={info.icon}
-            text={info.text}
-            href={info.href}
-          />
-        ))}
-      </div> */}
+        <div className="grid md:grid-cols-3 gap-8 justify-center">
+          {featureTileInfo.map((info, index) => (
+            <FeatureTile
+              key={index}
+              icon={info.icon}
+              text={info.text}
+              href={info.href}
+            />
+          ))}
+        </div>
+      </section>
 
       {/* {meritTileInfo.map((info, index) => (
         <MeritTile
